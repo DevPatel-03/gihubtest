ad4fba3f3ed2ebbac2c71ee7dad8a3273425700b
Dev Patel <dev.fluxbyte@gmail.com>
2024-03-14 14:43:01 +0530

    new file

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const age = 22;
 
-const myname = "dev patel"
+const myname1 = "dev patel"
 
 const dob = "03/12/2001"
 
 function sayName() {
-    console.log("my name is " + myname)
+    console.log("my name is " + myname1)
+}
+
+function isAbove18() {
+    return age >= 18
 }
